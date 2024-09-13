307d96076121b921582d2fc06956197523a36a56
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-09-13 04:25:07 +0200

    Bozza header

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,202 @@
 <script>
-  /*
-  Per importare ed utilizzare un componente dentro ad un altro devo Sempre seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-  */
-
-  // 1) Importazione del componente
- import AppHeader from './components/AppHeader.vue';
 
 export default {
+
   data() {
     return {
-      count: 0
+      message: 'Hello world',
+
+      links:[
+        {
+          text : 'CHARACTERS',
+          url : "href='#'",
+          active: false, 
+        },
+        {
+          text : 'COMICS',
+          url : "href='#'",
+          active: true,
+        },
+        {
+          text : 'MOVIES',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'TV',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'GAMES',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'COLLECTIBLES',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'CHARACTERS',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'VIDEOS',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'FANS',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'NEWS',
+          url : "href='#'",
+          active: false,
+        },
+        {
+          text : 'SHOP',
+          url : "href='#'",
+          active: false,
+        }
+      ]
     }
-  },
-  // 2) Dichiarazione del componente
-  components: {
-    AppHeader
   },
 
-  methods: {
-    incrementCount() {
-      this.count++;
-    }
-  }
+  methods:{
+
+  },
 }
+
 </script>
 
 <template>
-  <div>
-    <!-- 3) Utilizzo del componente -->
-    <AppHeader/>
-    <main>
-      <button @click="incrementCount()">
-        {{ count }}
-      </button>
-    </main>
-  </div>
+
+  <header>
+    <!-- sezione nera -->
+    <div class="black-section"></div>
+
+    <div class="container flex">
+      <!-- logo -->
+      <img class="logo" src=" " alt="DC">
+
+      <div class="col-60">
+        <ul>
+          <li v-for="(link, index) in links">
+            <a :href="link.url"> {{ link.text }} </a>  
+          </li>
+        </ul>
+      </div>
+
+    </div>
+
+  </header>
+
+  <main>
+
+    <div class="container">
+
+      <!-- content -->
+      <section>
+
+      </section>
+
+      <!-- prodotti -->
+      <section>
+        
+      </section>
+
+    </div>
+
+  </main>
+
+  <footer>
+
+    <div class="container">
+
+      <!-- footer top -->
+      <section>
+
+      </section>
+
+      <!-- footer bottom -->
+      <section>
+        
+      </section>
+
+    </div>
+
+    <!-- sezione nera -->
+    <div class="black-section"></div>
+
+  </footer>
+
 </template>
 
 <style scoped>
-main {
-  text-align: center;
+
+/* general */
+.flex{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
+
+img{
+  max-width: 100%;
+}
+
+/* header */
+
+.col-60{
+  width: 60%;
+}
+
+.logo{
+  width: 60px;
+}
+
+.black-section{
+height: 50px;
+width: 100%;
+background-color: #0E0E0E;
+}
+
+.active{
+  color: #0082F9;
+}
+
+ul{
+  padding: 0;
+
+  list-style: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li a{
+  font-family: "Roboto Condensed";
+  font-weight: bold;
+  font-size: 0.9rem;
+  text-decoration: none;
+  color: black;
+  display: inline-block;
+  padding: 10px;
+}
+
+a{
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+
+a .active{
+border-bottom: 2px solid #0082F9;
+}
+
+
 </style>
